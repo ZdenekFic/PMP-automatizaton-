672cbb8be06c54631b95ef0fa3176668107e0ca2
zdenek.fic <zdenek.fic@memos.cz>
2024-01-17 09:15:32 +0100

    test na vytvoření content bricku a k tomu test na smazání content bricku který vyhledá všechny vytvořené a smaže je

--- pmp tests folder/Tests/PMP-CB-draftCreation.spec.js
+++ pmp tests folder/Tests/PMP-CB-draftCreation.spec.js
@@ -1,30 +1,63 @@
-
 const { test, expect } = require("@playwright/test");
 const constants = require("../src/constants.js");
 import { LoginPage } from "../src/LoginPage.js";
 import { ContentBricks } from "../src/ContentBricks.js";
+
 //Login
 const username = constants.username;
 const password = constants.password;
 const baseURL = constants.baseURL;
 const loggedOUTpageTitle = constants.loggedOUTpageTitle;
-const name = constants.cbName
+const name = constants.cbName;
+const text = constants.cbText;
+const fieldName1 = constants.contentBrickFieldName1;
+const fieldName2 = constants.contentBrickFieldName2;
+const dropdownElement1 = constants.dropdownElement1;
+const dropdownElement2 = constants.dropdownElement2
 
-test("PMP Creation of CB", async ({ page }) => {
-  //Login
-  const login = new LoginPage(page);
-  await login.gotoLoginPage(baseURL);
-  await login.login(username,password);
-  await login.loginAssert();
+//Setting for non parralel running of tests
+test.describe.configure({ mode: 'serial' });
 
-  //main Function
-  const cb = new ContentBricks(page);
-  await cb.enterToCB();
-  await cb.formCB_General(name);
+test.describe('PMP Creation of CB', () => {
+  let login;
+  let cb;
+  let cb2;
+
+  test.beforeEach(async ({ page }) => {
+    //Login
+    login = new LoginPage(page);
+    await login.gotoLoginPage(baseURL);
+    await login.login(username,password);
+    await login.loginAssert();
+  });
+
+  test("PMP main Creation of CB", async ({ page }) => {
+    //main Functions
+    cb = new ContentBricks(page,dropdownElement1);
+    await cb.enterToCB();
+    await cb.formCB_General(name,text);
+    await cb.add_fields(fieldName1)
+
+    cb2 = new ContentBricks(page,dropdownElement2);
+    await cb2.add_fields(fieldName2)
+    
+    await cb.chooseCBState();
+  });
+
+  test("Check and delete", async ({ page }) => {
+    
+    //main Functions
+    cb = new ContentBricks(page,dropdownElement1,name);
+    await cb.checkCreatedCB();
+
+    
+  });
 
 
 
-  //LogOut
-  await login.logOut();
-  await login.logOutAssert(loggedOUTpageTitle);
+  test.afterEach(async ({}) => {
+    //LogOut
+    await login.logOut();
+    await login.logOutAssert(loggedOUTpageTitle);
+  });
 });
--- pmp tests folder/src/ContentBricks.js
+++ pmp tests folder/src/ContentBricks.js
@@ -1,16 +1,61 @@
-const { baseURL } = require("./constants");
+
+const { baseURL, cbName } = require("./constants");
+const { expect } = require("@playwright/test");
 
 exports.ContentBricks = class ContentBricks {
-  constructor(page) {
+  constructor(page,dropdownElement,mainName) {
     this.page = page;
+    this.mainName = mainName;
+    this.dropdownElement = dropdownElement;
     this.definitionsTab = page.getByRole('button', { name: 'Definitions' });
     this.contentBricksTab = page.locator('span').filter({ hasText: 'Content Bricks' }).first();
     this.addButton = page.getByRole('link', { name: 'Add' });
     this.generalFormName = page.getByLabel('Name', { exact: true });
     this.generalFormIdentifier = page.getByLabel('Identifier', { exact: true });
+    // Uasge types objects
     this.generalFormUsageTypesRedArrow = page.locator('.v-input__append-outer > .v-btn').first();
-    this.firstObjectUsageTypes = page.locator("//tbody/tr[1]/td[1]/div[1]/i[1]");
+    this.firstObjectInTable = page.locator("//tbody/tr[1]/td[1]/div[1]/i[1]");
+    this.secondObjectInTable = page.locator("//tbody/tr[2]/td[1]/div[1]/i[1]");
+    this.fifthObjectUsageInTable = page.locator("//tbody/tr[5]/td[1]/div[1]/i[1]");
     this.buttonUpdateUsageTypes = page.locator("//span[normalize-space()='Update Usage types']");
+    // Tags objects
+    this.generalFormTagsRedArrow = page.locator("(//i[@class='v-icon notranslate mdi mdi-upload theme--light'])[2]");
+    this.firstObjectInTableTags = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent v-dialog--scrollable']//div[@class='v-card__text']//tbody/tr[2]/td[1]");
+    this.secondObjectInTableTags = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent v-dialog--scrollable']//div[@class='v-card__text']//tbody/tr[3]/td[1]");
+    this.fifthObjectUsageInTableTags = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent v-dialog--scrollable']//div[@class='v-card__text']//tbody/tr[6]/td[1]");
+    this.buttonUpdateTags = page.locator("//span[normalize-space()='Update Tags']");
+
+    // Search identifiers
+    this.generalFormSearchIdentifierRedArrow = page.locator(".v-card__text > div:nth-child(3) > .pa-0 > div > .v-input > .v-input__append-outer > .v-btn");
+    this.firstOBjectInTableSearchIdentifiers = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent v-dialog--scrollable']//div[@class='v-card__text']//tbody/tr[1]/td[1]");
+    this.secondObjectInTableSearchIdentifiers = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent v-dialog--scrollable']//div[@class='v-card__text']//tbody/tr[2]/td[1]");
+    this.fifthOBjectInTableSearchIdentifiers = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent v-dialog--scrollable']//div[@class='v-card__text']//tbody/tr[5]/td[1]");
+    this.buttonUpdateSearchIdentifiers = page.locator("//span[normalize-space()='Update Search identifiers']");
+
+    // input for text to describe CB 
+    this.descriptionCB = page.locator("//div[@label='Description']//div[@class='ql-editor ql-blank']");
+
+    // Fields objects
+    this.addFieldButton = page.locator("(//button[@class='mt-3 v-btn theme--light elevation-2 v-size--default'])[1]");
+    this.fieldNameInput = page.locator("(//div[@class='col col-3'])[1] >> text='Name'");
+    this.fieldIdentifier = page.locator("//div[@class='col col-3']//div[@class='v-input theme--light v-text-field v-text-field--is-booted']//input");
+    this.fieldDataTypeButton = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent']//div[@class='v-card v-sheet theme--light']//div[@class='v-select__slot']");
+    this.elementDropdown = page.locator(`.v-list-item__title:has-text('${dropdownElement}')`);
+    this.switchIsMandatory = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent']//div[@class='v-card v-sheet theme--light']//div[@class='v-input--selection-controls__ripple']");
+    this.fieldAddButton = page.locator("//button[@class='error v-btn v-btn--flat v-btn--text theme--light v-size--default']//span[@class='v-btn__content'][normalize-space()='Add']");
+
+    //draft, active, suspended combobox
+    this.comboboxCBstate = page.getByRole('combobox').nth(1);
+    this.stateDraft = page.locator(" //div[@class='v-menu__content theme--light menuable__content__active v-autocomplete__content']//span[@class='status-chip-text'][normalize-space()='Draft']");
+    this.saveCBbutton = page.locator("//div[@class='detail-tab-menu-header-container']//button[2]")
+
+
+    // assertions objects
+    this.areaOfCbs = page.locator(`//span[@ui-test-data='overview-definitions-content-brick-grid-name']`);
+    this.deleteDraftButtton = page.locator("//div[@class='entity-detail-card v-card v-sheet theme--light']//i[@class='v-icon notranslate mdi mdi-delete theme--light']");
+    this.modalDeleteButton = page.locator("//div[@class='v-dialog v-dialog--active v-dialog--persistent']//div[@class='v-card v-sheet theme--light']//span[normalize-space()='Delete']");
+    
+
 
 
    
@@ -32,7 +77,7 @@ async enterToCB() {
 
 }
 
-async formCB_General(name) {
+async formCB_General(name,text) {
 
     //click and fill name
     await this.generalFormName.fill(name);
@@ -43,12 +88,86 @@ async formCB_General(name) {
     //add usage types -> opening modal window with usage types 
     await this.generalFormUsageTypesRedArrow.click();
     await this.page.waitForTimeout(1000);
-    await this.firstObjectUsageTypes.click();
+    await this.firstObjectInTable.click();
+    await this.secondObjectInTable.click();
+    await this.fifthObjectUsageInTable.click();
     await this.buttonUpdateUsageTypes.click();
 
+    // add tags 
+    await this.generalFormTagsRedArrow.click();
+    await this.page.waitForTimeout(1000);
+    await this.firstObjectInTableTags.click();
+    await this.secondObjectInTableTags.click();
+    await this.fifthObjectUsageInTableTags.click();
+    await this.buttonUpdateTags.click();
 
+    // add search identifiers
+    await this.generalFormSearchIdentifierRedArrow.click();
+    await this.page.waitForTimeout(1000);
+    await this.firstOBjectInTableSearchIdentifiers.click();
+    await this.secondObjectInTableSearchIdentifiers.click();
+    await this.fifthOBjectInTableSearchIdentifiers.click();
+    await this.buttonUpdateSearchIdentifiers.click();
+
+    // add some text to description
+    await this.descriptionCB.fill(text)
+
+}
+
+async add_fields(name) {
+
+    await this.addFieldButton.click();
+    await this.page.waitForTimeout(1000);
+    await this.fieldNameInput.fill(name);
+    await this.fieldIdentifier.click();
+    await this.fieldDataTypeButton.click();
+    await this.page.waitForTimeout(1000);
+    await this.elementDropdown.click();
+    await this.switchIsMandatory.click();
+    await this.fieldAddButton.click();
+
+}
+
+async chooseCBState() {
+
+    await this.comboboxCBstate.click();
+    await this.stateDraft.click()
+    await this.saveCBbutton.click();
 
 }
 
 
-};
+async checkCreatedCB() {
+    //click on Definitons tab
+    await this.definitionsTab.click();
+
+    //click on Definitions/Content Bricks TAB
+    await this.contentBricksTab.click();
+    await this.page.waitForTimeout(4000);
+
+    let elements = await this.page.$$(`body >> text=${this.mainName}`);
+
+    for (let i = 0; i < elements.length; i++) {
+        const elementHandle = elements[i];
+        const elementText = await elementHandle.innerText();
+
+        if (elementText === this.mainName) {
+            await elementHandle.click();
+            await this.deleteDraftButtton.click();
+            await this.modalDeleteButton.click();
+            await this.page.waitForTimeout(2000);
+
+            // Fetch the latest elements after the deletion
+            elements = await this.page.$$(`body >> text=${this.mainName}`);
+            await this.page.waitForTimeout(2000);
+
+            // Reset the index to recheck the elements
+            i = -1;
+       }
+        
+      }
+
+}};
+
+
+
